4983288cc16bb57da8e8852cf4b5bba58e934a98
Marcelo Garcia Bassichetti <bassidevand@gmail.com>
2025-02-04 13:56:47 -0300

    no servico

---
+++ src/main/java/com/library/libraryapi/controller/mapper/AutorMapper.java
@@ -0,0 +1,20 @@
+package com.library.libraryapi.controller.mapper;
+
+import com.library.libraryapi.controller.dto.AutorDTO;
+import com.library.libraryapi.model.Autor;
+import org.mapstruct.InheritConfiguration;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface AutorMapper {
+
+    AutorMapper INSTANCE = Mappers.getMapper(AutorMapper.class);
+
+    Autor toEntity(AutorDTO dto);
+
+    @InheritInverseConfiguration
+    AutorDTO toDTO(Autor entity);
+
+}
